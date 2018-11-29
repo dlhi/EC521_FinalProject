f7b11d52f2a0a4dbbe3b305bb427150efedd15a3
agonzaga <agonzaga@bu.edu>
2018-11-29 04:09:28 -0500

    vulnerable files

---
+++ vulnerable/exec_vuln.py
@@ -0,0 +1,14 @@
+"""
+This vulnerable program uses the Python exec function which takes in a string and executes it using python3
+"""
+def main():
+    a = input("Enter a command\n")
+    b = 1 + 1                       # Fillers
+    c = 1 + b                       # Fillers
+    d = b - c                       # Fillers
+    exec(a)
+
+
+
+if __name__ == '__main__':
+    main()
