f903ad9bd0bd4248bb57ed55ee9f40fd36cb7582
Nicolas Hunt <nhunt@dhcp-wifi-8021x-155-41-96-254.bu.edu>
2018-12-09 16:24:17 -0500

    uses list of tuples as input

--- Static Analysis/format-config.py
+++ Static Analysis/format-config.py
@@ -1,5 +1,53 @@
-# format pytaint configure file
-'''
+"""
+This module automatically formats a pytaint configure file, given processed bandit output as input
+"""
+def format(bandit_out):
+	mtmap = {"B605": "ShellMerit", "B608": "SQLiMerit", "bandit_id3" : "HtmlMerit", "B102": "ShellMerit"}
+	f = open("test.json", "w+")
+
+	merit = []
+	source = []
+	sink = []
+
+	for i in bandit_out:
+		merit.append(mtmap[i[0]])
+		source.append(i[1])
+		sink.append(i[2])
+
+	num_sinks = len(sink)
+	num_srcs = len(source)
+
+
+	conf = ""
+	conf += '''{\n\t"sinks": ['''
+	for i in range(0, num_sinks):
+		conf += '''\n\t{{"merit": "{}"}},\n\t "{}"'''.format(merit[i], sink[i])
+
+		if(i != num_sinks-1):
+			conf += ","
+		else:
+			conf += "],\n\t"
+
+	conf += '''"sources": ['''
+	for j in range(0, num_srcs):
+		conf += '''\n\t"{}"'''.format(source[j])
+
+		if(j != num_srcs-1):
+			conf += ","
+		else:
+			conf += "],\n}\n"
+
+
+	# conf = '''{{\n\t"cleaners": [\n\t{{"merit": {}}},\n\t "pipes.quote"],\n\t"sinks": [\n\t{{"merit": {}}},\n\t {}],\n\t"sources": [\n\t{}]\n}}\n'''.format(merit, merit, sink, source)
+
+	f.write(conf)
+	f.close()
+
+
+"""
+Test function - input is a list of tuples of strings
+
+Example Output Config file:
 {
   "cleaners": [
     {"merit": "ShellMerit"},
@@ -21,54 +69,12 @@
      "hashlib.md5"
     ]
 }
-'''
-
-def format(sink, source, bandit_id):
-	f = open("test.json", "w+")
-	mtmap = {1: "ShellMerit"}
-	# merit = mtmap[bandit_id]
-	merit = ["ShellMerit", "PickleMerit"]
-
-	num_sinks = len(sink)
-	num_srcs = len(source)
 
 
-	conf = ""
-	conf += """{\n\t"sinks": ["""
-	for i in range(0, num_sinks):
-		conf += '''\n\t{{"merit": "{}"}},\n\t "{}"'''.format(merit[0], sink[i])
-
-		if(i != num_sinks-1):
-			conf += ","
-		else:
-			conf += "],\n\t"
-
-	conf += """"sources": ["""
-	for j in range(0, num_srcs):
-		conf += '''\n\t"{}"'''.format(source[j])
-
-		if(j != num_srcs-1):
-			conf += ","
-		else:
-			conf += "],\n}\n"
-
-
-	# conf = '''{{\n\t"cleaners": [\n\t{{"merit": {}}},\n\t "pipes.quote"],\n\t"sinks": [\n\t{{"merit": {}}},\n\t {}],\n\t"sources": [\n\t{}]\n}}\n'''.format(merit, merit, sink, source)
-
-	f.write(conf)
-	f.close()
-
-
-
-
-
-def test1():
-	format("raw_input", "os.system", 1)
-
-def test2():
-	format(["Handler.send_response", "Handler.send_response", "pickle.loads", "commands.getoutput"], ["src1", "src2", "src3"], [2, 1, 5])
-
+"""
+def test():
+	format([("B605", "src1", "os.system", "/home", "45"), ("B608", "src2", "sql_something", "/hom/ls", "812")])
 
 
 if __name__ == '__main__':
-	test2()
+	test()
