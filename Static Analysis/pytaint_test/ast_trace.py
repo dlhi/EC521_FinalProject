3b5d8c5912c6eb9320232ba9f6c246b19d6bcefd
David <dlhi@bu.edu>
2018-12-09 16:40:49 -0500

    ast implementation

---
+++ Static Analysis/pytaint_test/ast_trace.py
@@ -0,0 +1,47 @@
+import ast
+import sys
+
+class GlobalUseCollector(ast.NodeVisitor):
+    def __init__(self, name):
+        self.name = name
+        # track context name and set of names marked as `global`
+        self.context = [('global', ())]
+
+    def visit_FunctionDef(self, node):
+        self.context.append(('function', set()))
+        self.generic_visit(node)
+        self.context.pop()
+
+    def visit_ClassDef(self, node):
+        self.context.append(('class', ()))
+        self.generic_visit(node)
+        self.context.pop()
+
+    def visit_Lambda(self, node):
+        # lambdas are just functions, albeit with no statements
+        self.context.append(('function', ()))
+        self.generic_visit(node)
+        self.context.pop()
+
+    def visit_Global(self, node):
+        assert self.context[-1][0] == 'function'
+        self.context[-1][1].update(node.names)
+
+    def visit_Name(self, node):
+        ctx, g = self.context[-1]
+        if node.id == self.name :
+            print('{} used at line {}'.format(node.id, node.lineno))
+
+indiv_file = sys.argv[1]
+with open(indiv_file, 'r') as myfile:
+    data = myfile.read()
+myfile.close()
+
+# print "Got here!"
+readin = ast.parse(data)
+# print data
+for node in ast.walk(readin):
+    if isinstance(node, ast.FunctionDef):
+        print(node.name)
+
+a = GlobalUseCollector('s').visit(readin)
---
+++ Static Analysis/pytaint_test/system_vuln.py
@@ -0,0 +1,40 @@
+#!/home/david/Desktop/EC521_Project/pytaint/Python-2.7.5-pytaint/python
+
+"""
+This vulnerable program uses the Python os.system function which takes in a string, opens a shell and executes the command
+
+Works with Pytaint - taint error triggered
+"""
+
+import os
+import taint
+import pipes
+
+class ShellMerit(Merit):
+  '''A string has been cleaned for usage as a shell parameter'''
+  propagation = Merit.FullPropagation
+
+# input() for user input does not work with taint analysis
+
+def fake_sanitize(s):
+    return s
+
+def inputed():
+    return raw_input()
+
+def main():
+    taint.enable('simple_vuln_config.json')
+
+    e = inputed()
+    f = fake_sanitize(e)
+    print("Printing e: ", e)
+
+    b = 1 + 1                       # Fillers
+    c = 1 + b                       # Fillers
+    d = b - c                       # Fillers
+    os.system(f)
+
+if __name__ == '__main__':
+    main()
+    x = 1 + 2
+    y = 2 + x 
